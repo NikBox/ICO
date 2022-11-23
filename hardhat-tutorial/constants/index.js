2a2e6a06a2ce359454e7a8a697e2bb9aac77dedf
NikBox <nik7.box@gmail.com>
2022-11-23 12:36:47 +0100

    first commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xdac1B4E4f5F53e2d412d15E22cbF32a4be936b06";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
